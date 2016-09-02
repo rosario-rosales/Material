382998d910778edbeeea1f6d9bbf73477bacbe65
rosario-rosales <rr.rosario.rosales@gmail.com>
2016-09-02 18:46:27 -0500

    Revisar
    Se agrego varias carpetas con material para revisar

---
+++ Prueba/plugins/lightbox/js/lightbox.js
@@ -0,0 +1,209 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+<title>Parked-Domain.org</title>
+
+<meta name="keywords" content="cash advance debt consolidation insurance parked-domain.org" />
+
+<meta name="description" content="Find Cash Advance, Debt Consolidation and more at Parked-Domain.org. Get the best of Insurance or Free Credit Report, browse our section on Cell Phones or learn about Life Insurance. Parked-Domain.org is the site for Cash Advance." />
+
+<script src='http://code.jquery.com/jquery-latest.min.js' type='text/javascript'></script>
+<script language='JavaScript' src='/js/standard.js?rte=1&tm=2&dn=parked-domain.org&tid=1026'></script>
+
+<meta name='google' value='notranslate' />
+<script type='text/javascript' language='JavaScript' src='/js/google_caf.js?rte=1&tm=2&dn=parked-domain.org&tid=1026'></script>
+<script type='text/javascript' language='JavaScript' src='http://www.google.com/adsense/domains/caf.js'></script>
+
+<script type='text/javascript'>
+var pageOptions =
+{
+    'domainRegistrant' : 'as-drid-2421601518898051',
+    'relatedSearchBaseUrl': 'http://parked-domain.org/?ac=2&slt=8&slr=1&lpt=1',
+    'resultsPageBaseUrl': 'http://parked-domain.org/?ac=2&slt=8&slr=1&lpt=1',
+    'pageLoadedCallback': google_callback,
+    'pubId': 'dp-demandmedia_test_js',
+    'channel': '000001',
+    'terms': '',
+    'optimizeTerms': true,
+    'uiOptimize' : true, 
+    'adtest': 'off',
+    'hl': 'es'
+};
+
+var rsblock1 =
+{
+    'container': 'searchLinks',
+    'type': 'relatedsearch',
+    'number': 8,
+    'colorBackground': '#0D0E10',
+    'colorAttribution': '#ACB2B2',
+    'fontFamilyAttribution': 'arial',
+    'fontSizeAttribution': 14,
+    'attributionBold': 'false',
+    'fontFamily': 'arial',
+    'fontSizeTitle': 17,
+    'colorTitleLink': '#E6E8E7',
+    'rolloverLinkColor': '#51C2E7',
+    'noTitleUnderline': true,
+    'titleBold': true,
+    'lineHeightTitle': 26,
+    'adIconUrl': '//afs.googleusercontent.com/dp-demandmedia/t1026/blue/bullet-arrow.png',
+    'adIconWidth': 12,
+    'adIconHeight': 21,
+    'adIconSpacingAbove': 2,
+    'adIconSpacingBefore': 20,
+    'adIconSpacingAfter': 15,
+    'verticalSpacing': 24,
+    'colorAdSeparator': '#1B2023'
+};
+
+var searchboxBlock =
+{
+    'container': 'searchbox',
+    'type': 'searchbox',
+    'width': '275px',
+    'colorBackground': 'transparent',
+    'colorSearchButton': '#26ADE4',
+    'colorSearchButtonText': '#fff',
+    'fontSizeSearchInput': 16,
+    'fontSizeSearchButton': 16,
+    'hideSearchInputBorder': true,
+    'hideSearchButtonBorder': false
+};
+
+
+var parkingData = 
+{
+    'pageType': '1',
+    'googleStatus': 'Clean',
+    'cacheKey': '00000000-0000-0000-0000-000000000000',
+    'clientIPHash': '-901650074',
+    'userAgentHash': '-1347450078',
+    'time': '9/2/2016 8:55:11 AM',
+    'ticks': '636084033119172091',
+    'domainName': 'parked-domain.org',
+    'searchText': '',
+    'actionCode': 'InitialView',
+    'adNetworkID': '260',
+    'moduleID': '29',
+    'resultLinkType': '5',
+    'webLinkType': 'GoogleWeb',
+    'vipIP': '98.124.243.48',
+    'sessionID': 'db192a62-b611-4ba3-904a-ef93f5886fda',
+    'visitorID': '46919c5a-8882-4dac-b8fd-b2bb1fc1a8f8',
+    'ippiClassification': '4',
+    'requestGuid': '7fb2b096-c247-47a3-87dc-20848d6c4c03',
+    'searchLinkType': 'Unknown',
+    'templateID': '1026',
+    'themeID': '220322',
+    'pageUrl': '/',
+    'searchLinkRank': '0',
+    'referrer': '',
+    'referrerSearchText': '',
+    'subID': '0',
+    'searchLinkAreas': 
+    [ 
+        {
+            'maxCount': 8,
+            'divID': 'sec_8',
+            'class': 'searchLinks',
+            'headerText': 'Related Searches'
+        }
+    ],
+    'apiIteration': 0,
+    'mobileBrowserType': 5,
+    'adLineFormat': 3,
+    'wClass': 'wr',
+    'terms': '',
+    'adHeader': 'Sponsored Listings'
+};
+
+new google.ads.domains.Caf(pageOptions, rsblock1, searchboxBlock);
+</script>
+
+
+<title>Mobile Template v2</title>
+<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8"/>
+<meta name="viewport" content="width=320" />
+<link href="/css/style.css?rte=1&tm=2&dn=parked-domain.org&tid=1026&def=Akamai%3aHostingURL%3dhttp%3a%2f%2fi.nuseek.com%7cParking%3aSkinPath%3dblue%7cPrimaryColor%3d%2361c8e9%7cSecondaryColor%3d%23ade7fe%7cBulletLander%3aImagePath%3d%2fimages%2fblue%2fbullet-arrow.png%7cBulletResults%3aImagePath%3d%2fimages%2fblue%2fbullet-arrow-mini.png%7cSearchButton%3aImagePath%3d%2fimages%2fblue%2fbutton-search-text.png" media="all" type="text/css" rel="stylesheet"/>
+</head>
+
+<body id="lander"  class='caf'>
+
+<!--
+=================================================
+** START DEBUG OUTPUT                          **
+=================================================
+             Version: 3.7.171.1
+     Logging_Version: 3.6
+           Webserver: 2402B
+           Client_IP: 190.238.144.91
+              VIP_IP: 98.124.243.48
+           HTTP_Host: parked-domain.org
+         Domain_Name: parked-domain.org
+  Total_Process_Time: 21ms
+       MemCache_Time: 5ms (24%)
+       Database_Time: 13ms (62%)
+            API_Time: 1ms (6%)
+=================================================
+** END DEBUG OUTPUT                            **
+=================================================
+-->
+
+
+<form id="parking_form" method="get" action="/default.pk" runat="server">
+
+<div class="container">
+
+    <div class="content">
+    
+        <div id='searchLinks' class='searchLinks'></div> 
+    
+    </div>
+
+    <div class="footer">
+
+        <div id='searchbox' class='searchBox'>
+</div>
+	 	    
+    
+    </div>
+                     
+</div>   
+
+<div class='legal'><a href='http://whoisprivacyprotect.com/terms/privacy.html' target='_blank'>Privacy Policy</a></div>
+
+<div class='adnet'>
+</div>
+
+<!-- Special script for Interlinx/Adnet.. removes our PP link and adds theirs -->
+<script type='text/javascript'>
+$(document).ready(function() {
+	if (pageOptions.domainRegistrant == "as-drid-2237586301058971") {
+		$(".legal").remove();
+
+		var script = document.createElement('script');
+		script.type = "text/javascript";
+		script.src = "https://adnet-contact-links.surge.sh/contact-links.min.js";
+		$(".adnet").append(script);
+
+		var ahref = $("<a>Domain Asset Group Advertising Notice</a>").attr("href", "http://www.domainassetgroup.com/").attr("style", "color:#A8A7A7; font-size:12px; text-decoration:none;").attr("target", "_blank");
+		$(".hotmedia").append(ahref);
+	}
+});
+
+$(window).on("load", function() {
+    $("div:contains('Unsubscribe')").attr("class", "adnet");
+});
+
+</script>
+
+<div class='hotmedia'>
+</div>
+
+
+</form>    
+</body>
+</html>
